a2e128676389fd5bf3e282d92a9f9b11079b3a7d
Abhishekwmg <ak89471@gmail.com>
2024-03-03 21:00:54 +0530

    Sign In Step 2

---
+++ app/main.js
@@ -0,0 +1,6 @@
+// const passEye = document.querySelector("#pass-eye");
+// const passKey = document.querySelector('#pass-key')
+// console.log(passKey);
+// passEye.addEventListener('click', (e) => {
+//     e.preventDefault();
+// })
